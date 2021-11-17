a0ead9ac4e3af82d3fb894d2061669042ea24512
Doug <douglas_greenwood@hotmail.com>
2021-11-17 10:44:04 +0000

    Finished mobile nav

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function classToggle() {
+  const navs = document.querySelectorAll(".navbar__items");
+
+  navs.forEach((nav) => nav.classList.toggle("navbar__toggleShow"));
+}
+
+document
+  .querySelector(".navbar__link-toggle")
+  .addEventListener("click", classToggle);
